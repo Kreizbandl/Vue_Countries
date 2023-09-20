34a84ecc362d512d67e5c88b5c52258ab1971b05
Manuel T <manuelthuer72@gmail.com>
2023-09-20 13:08:40 +0200

    contact form style, error, diasbled button, submit

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,43 +1,109 @@
 <template>
   <!-- Überschrift -->
   <h1>Feedback Form</h1>
-  <!-- Feedback Formular-->
-  <form>
-    <!-- Eingabefeld für den Namen mit beschreibendem Label -->
-    <div>
-      <label for="name">Name:
-        <input ref="nameInput" type="text" id="name" name="name" aria-label="Enter your name"/>
-      </label>
-    </div>
-    <!-- Eingabefeld für die Email mit beschreibendem Label -->
-    <div>
-      <label for="email">Email:
-        <input type="text" id="email" name="email" aria-label="Enter your email" />
-      </label>
-    </div>
-    <!-- Textbereich für die Nachricht mit beschreibendem Label -->
-    <div>
-      <label for="message">Message:
-        <textarea type="text" id="message" name="message" aria-label="Enter a message"></textarea>
-      </label>
-    </div>
-    <!-- Button zum Absenden des Formulars mit beschreibendem Label -->
-    <button type="submit" aria-label="Submit">Submit</button>
-  </form>
+  <div v-if="!isFormSubmitted" class="contact">
+    <!-- Feedback Formular-->
+    <form @submit.prevent="handleSubmit">
+      <!-- Eingabefeld für den Namen mit beschreibendem Label -->
+      <div>
+        <label for="name">Name:
+          <input v-model="name" ref="nameInput" type="text" id="name" name="name" aria-label="Enter your name"
+          :style="{ borderColor: nameEmpty && nameInputBlurred ? 'red' : 'inherit' }" @blur="nameInputBlurred = true" />
+        </label>
+      </div>
+      <!-- Eingabefeld für die Email mit beschreibendem Label -->
+      <div>
+        <label for="email">Email:
+          <input v-model="email" type="text" id="email" name="email" aria-label="Enter your email" 
+          :style="{ borderColor: emailEmpty && emailInputBlurred ? 'red' : 'inherit' }" @blur="emailInputBlurred = true" />
+        </label>
+      </div>
+      <!-- Textbereich für die Nachricht mit beschreibendem Label -->
+      <div>
+        <label for="message">Message:
+          <textarea v-model="message" type="text" id="message" name="message" aria-label="Enter a message"
+          :style="{ borderColor: messageEmpty && messageInputBlurred ? 'red' : 'inherit' }" @blur="messageInputBlurred = true" ></textarea>
+        </label>
+      </div>
+      <!-- Button zum Absenden des Formulars mit beschreibendem Label -->
+      <button id="submit" type="submit" aria-label="Submit" :disabled="!isFormValid">Submit</button>
+    </form>
+  </div>
+  <div v-else>
+    <p>Thank you for your message!</p>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'ContactComponent',
+  data(){
+    return{
+      name: '',
+      email: '',
+      message: '',
+      nameInputBlurred: false,
+      emailInputBlurred: false,
+      messageInputBlurred: false,
+      isFormSubmitted: false
+    };
+  },
+  computed: {
+    isFormValid() {
+      return this.name.trim() !== '' && this.email.trim() !== '' && this.message.trim() !== '';
+    },
+    nameEmpty() {
+      return this.name === '';
+    },
+    emailEmpty() {
+      return this.email === '';
+    },
+    messageEmpty() {
+      return this.message === '';
+    },
+  },
   mounted() {
     // Nachdem die Komponente montiert wurde, den Fokus auf das Name-Eingabefeld setzen
     this.$refs.nameInput.focus();
+  },
+  methods: {
+    handleSubmit(){
+      console.log("Form: ", this.name, this.email, this.message);
+      this.isFormSubmitted = true;
+    }
   }
 }
 </script>
 
 <style scoped>
+.contact {
+    margin: 0 auto;
+    text-align: center;
+}
 form {
-  text-align: center;
+    display: inline-block;
+    width: 400px;
+    flex-direction: column;
+}
+#name,
+#email,
+#message {
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #333333;
+    border-radius: 3px;
+    margin-bottom: 10px;
+    margin-top: 5px;
+}
+
+textarea {
+    height: 150px;
+}
+
+#submit {
+    width: 100px;
+    height: 30px;
+    border: 1px solid #333333;
+    border-radius: 3px;
 }
 </style>
